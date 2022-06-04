463a5f7a11ba9ddf883454e729650021aa9cd51f
oleksii.shylo <oleksii.shylo@pm.bet>
2022-06-05 00:48:16 +0300

    open port for http

--- terraform/network.tf
+++ terraform/network.tf
@@ -70,3 +70,30 @@ resource "aws_security_group" "ssh" {
     create_before_destroy = true
   }
 }
+
+resource "aws_security_group" "http" {
+  name        = "http"
+  description = "http from my PC"
+  vpc_id      = aws_vpc.prod_like.id
+
+  ingress {
+    description = "http from my PC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["213.207.186.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
