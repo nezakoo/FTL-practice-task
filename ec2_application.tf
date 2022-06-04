c7a845654c06c93c9db2cd1ed8a001bf0cee2662
oleksii.shylo <oleksii.shylo@pm.bet>
2022-06-04 14:46:44 +0300

    create ec2 for the application

---
+++ ec2_application.tf
@@ -0,0 +1,25 @@
+module "ec2_application" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "4.0.0"
+  name                        = "application"
+  ami                         = data.aws_ami.ubuntu20_04.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.prod_like_public.id
+  associate_public_ip_address = "true"
+  key_name                    = aws_key_pair.ec2_application_key.key_name
+  vpc_security_group_ids      = [aws_security_group.ssh.id]
+
+  tags = merge(local.tags, {
+    Name = "application_ec2"
+  })
+
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 100
+    }
+  ]
+  volume_tags = merge(local.tags, {
+    Name = "application_ebs"
+  })
+}
